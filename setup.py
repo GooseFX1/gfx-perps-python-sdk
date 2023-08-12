ca39c48ea2e18421bd8d659586fd2b874fdeba87
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2023-08-12 10:35:51 +0530

    updates sdk version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gfx_perp_python_sdk',
-    version='0.0.8',
+    version='0.0.9',
     url='https://github.com/GooseFX1/gfx-perp-python-sdk',
     author='Shashank Shekhar',
     author_email='shashank@goosefx.io',
