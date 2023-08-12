f62f4ff57eb7c5c22c36f876af975c96ed4afabc
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2023-08-12 11:09:04 +0530

    new package name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Gfx_perps_sdk',
+    name='gfx_perp_sdk',
     version='0.0.1',
     url='https://github.com/GooseFX1/gfx-perps-python-sdk',
     author='Shashank Shekhar',
     author_email='shashank@goosefx.io',
     description='Perp python sdk',
-    packages=['Gfx_perps_sdk'],    
+    packages=['gfx_perp_sdk'],    
     install_requires=[
         'pytest'
     ],
