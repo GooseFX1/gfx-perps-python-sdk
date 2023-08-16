9210fb5e94434c4ccdbda3708a465ef0e60e2204
Shashank Shekhar <shashank@Shashanks-MacBook-Pro.local>
2023-08-16 14:29:56 +0530

    added support for descriiption

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='gfx_perp_sdk',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/GooseFX1/gfx-perps-python-sdk',
     author='Shashank Shekhar',
     author_email='shashank@goosefx.io',
@@ -11,4 +14,6 @@ setup(
     install_requires=[
         'pytest'
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
